b30e08ca9726f9f2a3c9c977036c749deadc524a
SpookyGlitches <earljseph@gmail.com>
2020-11-30 19:01:15 +0800

    ok peko

--- smart_pet_feeder.sql
+++ smart_pet_feeder.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 22, 2020 at 05:09 PM
+-- Generation Time: Nov 30, 2020 at 12:00 PM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.2.27
 
@@ -25,27 +25,44 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `feeding_logs`
+--
+
+CREATE TABLE `feeding_logs` (
+  `id` int(11) NOT NULL,
+  `pet_id` int(11) NOT NULL,
+  `date_time` datetime DEFAULT current_timestamp(),
+  `duration` int(11) NOT NULL,
+  `status` enum('SUCCESS','FAIL') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `pets`
 --
 
 CREATE TABLE `pets` (
   `id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
+  `socket_address` varchar(60) NOT NULL,
   `name` varchar(255) NOT NULL,
   `details` varchar(255) DEFAULT NULL,
-  `color` varchar(255) NOT NULL
+  `color` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+-- --------------------------------------------------------
+
 --
--- Dumping data for table `pets`
+-- Table structure for table `schedules`
 --
 
-INSERT INTO `pets` (`id`, `user_id`, `name`, `details`, `color`) VALUES
-(27, 13, 'fff', 'ffff', '#ff595e'),
-(28, 13, '333', '3331', '#ff595e'),
-(29, 13, '22', '2', '#ff595e'),
-(30, 13, 'eee', 'eeddd', '#8ac926'),
-(31, 13, 'dd', 'ddd', '#8ac926');
+CREATE TABLE `schedules` (
+  `id` int(11) NOT NULL,
+  `pet_id` int(11) NOT NULL,
+  `time` time NOT NULL DEFAULT current_timestamp(),
+  `duration` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -61,21 +78,29 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `users`
---
-
-INSERT INTO `users` (`id`, `email`, `hash`, `salt`) VALUES
-(13, '123@usc', 'c4d7f551518e0efaffe1a2ece2e65061f0ffd161e90708ee09445a03fba904dff4a372e4d6a73818d1c052a17fd0bf30d8908c20b8be4c8e85cca2d5ff08cf45', '2955deba798cd31fc3ebde4dfe760a1d45709f557b7cac29a9179f21ae9cce76');
-
---
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `feeding_logs`
+--
+ALTER TABLE `feeding_logs`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `pet_id` (`pet_id`);
 
 --
 -- Indexes for table `pets`
 --
 ALTER TABLE `pets`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_user_id` (`user_id`);
+
+--
+-- Indexes for table `schedules`
+--
+ALTER TABLE `schedules`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `pet_id` (`pet_id`);
 
 --
 -- Indexes for table `users`
@@ -88,16 +113,50 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `feeding_logs`
+--
+ALTER TABLE `feeding_logs`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
 -- AUTO_INCREMENT for table `pets`
 --
 ALTER TABLE `pets`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=202;
+
+--
+-- AUTO_INCREMENT for table `schedules`
+--
+ALTER TABLE `schedules`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `feeding_logs`
+--
+ALTER TABLE `feeding_logs`
+  ADD CONSTRAINT `feeding_logs_ibfk_1` FOREIGN KEY (`pet_id`) REFERENCES `pets` (`id`);
+
+--
+-- Constraints for table `pets`
+--
+ALTER TABLE `pets`
+  ADD CONSTRAINT `fk_user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+
+--
+-- Constraints for table `schedules`
+--
+ALTER TABLE `schedules`
+  ADD CONSTRAINT `schedules_ibfk_1` FOREIGN KEY (`pet_id`) REFERENCES `pets` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
