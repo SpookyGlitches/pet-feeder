a7771cd06cc43063f253ba82c33b94237bd68897
SpookyGlitches <earljseph@gmail.com>
2020-12-29 18:39:11 +0800

    .

--- index.js
+++ index.js
@@ -1,115 +1,34 @@
 require('dotenv').config()
 
 const express = require('express');
-const session = require("express-session");
-const flash = require('connect-flash');
-const bodyParser = require('body-parser')
-const expressLayouts = require('express-ejs-layouts')
-const http = require('http');
-// const WebSocket = require('ws');
+// const session = require("express-session");
+// const flash = require('connect-flash');
+// const bodyParser = require('body-parser')
+// const expressLayouts = require('express-ejs-layouts')
+// const http = require('http');
+// // const WebSocket = require('ws');
 
-const uuiders = require('uuid');
+// const uuiders = require('uuid');
 
-const passportConfig = require('./config/passport');
+// const passportConfig = require('./config/passport');
 
 
-const homeRouter = require('./routes/home');
-const accountsRouter = require('./routes/accounts');
+// const homeRouter = require('./routes/home');
+// const accountsRouter = require('./routes/accounts');
 
-// var job = require('./cron.js');
+// // var job = require('./cron.js');
 
 
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.set('view engine', 'ejs');
-app.set('layout', 'layouts/layout')
-
-app.use(express.static('public'))
-app.use('/static', express.static('public'))
-// app.use(helmet());
-
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json())
-app.use(expressLayouts)
-
-app.set('trust proxy', true)
-app.use(session({ secret: "C4$s", resave: false, saveUninitialized: false }));
-app.use(flash());
-
-app.use(passportConfig.initialize());
-app.use(passportConfig.session());
-
-
-
-
-app.get('/',function (req,res){
-  if(req.isAuthenticated()) res.redirect('/home');
-  else res.render('index/first-vist');
+app.get('/',function(req,res){
+  res.send("Hello");
 })
 
 
-// app.use("/feed",wsRouter);
-app.use('/home', homeRouter);
-app.use('/accounts', accountsRouter);
-
-// const server = http.createServer(app);
-// const wss = new WebSocket.Server({ server, clientTracking: true });
-
-// server.on('upgrade')
-// const db = require('./config/database');
-
-// // let rooms = {};
-// // wss.on('connection', function (ws, req) {
-// //   // clients[req.url.substring(1)] = ws;
-// //   let uuid = uuiders.v4();
-// //   const leave = room => {
-// //     console.log("Start of leave function: " + Object.keys(rooms).length);
-// //     if (!rooms[room][uuid]) return; // not present: do nothing
-// //     if (Object.keys(rooms[room]).length === 1) delete rooms[room]; // if the one exiting is the last one, destroy the room
-// //     else delete rooms[room][uuid]; // otherwise simply leave the room
-// //     console.log("Someone tried to leave, we now have:" + Object.keys(rooms).length);
-
-// //   };
-// //   ws.on('message', function (dataer) {
-// //     let data = JSON.parse(dataer);
-// //     console.log(dataer);
-// //     if (data.meta == 'join') {
-// //       console.log("Someone tried to join");
-// //       if (!rooms[data.uuid]) {
-// //         console.log("someone created");
-// //         rooms[data.uuid] = {}; // create the room
-// //       }
-// //       if (!rooms[data.uuid][uuid]) {
-// //         console.log("Someone joined");
-// //         rooms[data.uuid][uuid] = ws;
-// //       }  // join the room
-// //     } else if (data.meta === "leave") {
-// //       leave(data.uuid);
-// //     } else if (!data.meta) {
-// //       if (data.type == 'res') {
-// //         console.log('Storing to database now!');
-// //         db.query("INSERT INTO feeding_logs (pet_id,duration,status) VALUES (?,?,?)", [data.id, data.duration, "SUCCESS"], (err, results) => {
-// //           if (err) console.log(err);
-// //           else console.log('Success');
-// //         })
-// //       } else if (data.type == 'req') {
-// //         console.log("Requesting to feed");
-// //         Object.entries(rooms[data.uuid]).forEach(([, sock]) => sock.send(JSON.stringify(data)));
-// //       }
-// //     }
-
-// //   });
-// //   ws.on("close", () => {
-// //     Object.keys(rooms).forEach(room => leave(room));
-// //   });
-
-// // })
-
-
 app.listen(port, function(){
-  // job.start();
   console.log("App running at port " + port);
 })
 
