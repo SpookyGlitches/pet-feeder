d80be4161e21f3c182501a909e021f60d2c99b51
SpookyGlitches <earljseph@gmail.com>
2020-12-29 18:51:20 +0800

    .

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require('express');
 // const session = require("express-session");
 // const flash = require('connect-flash');
 // const bodyParser = require('body-parser')
-// const expressLayouts = require('express-ejs-layouts')
+const expressLayouts = require('express-ejs-layouts')
 // const http = require('http');
 // // const WebSocket = require('ws');
 
@@ -22,6 +22,10 @@ const express = require('express');
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.set('view engine', 'ejs');
+app.set('layout', 'layouts/layout')
+app.use(expressLayouts)
 
 app.get('/',function(req,res){
   res.render('index/first-visit');
