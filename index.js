1b31c65737693f39287c05cda2df56091b148b8f
SpookyGlitches <earljseph@gmail.com>
2020-12-29 19:24:01 +0800

    .

--- index.js
+++ index.js
@@ -1,21 +1,13 @@
 require('dotenv').config()
 
 const express = require('express');
-// const session = require("express-session");
-// const flash = require('connect-flash');
-// const bodyParser = require('body-parser')
+const session = require("express-session");
+const flash = require('connect-flash');
+const bodyParser = require('body-parser')
 const expressLayouts = require('express-ejs-layouts')
 // const http = require('http');
 // // const WebSocket = require('ws');
-
 // const uuiders = require('uuid');
-
-// const passportConfig = require('./config/passport');
-
-
-// const homeRouter = require('./routes/home');
-// const accountsRouter = require('./routes/accounts');
-
 // // var job = require('./cron.js');
 
 const homeRouter = require('./routes/home');
