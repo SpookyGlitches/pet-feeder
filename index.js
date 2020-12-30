d9401be8e0d32f7558b20c8f7393c0ee3e083fc2
SpookyGlitches <earljseph@gmail.com>
2020-12-30 13:53:06 +0800

    .

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const bodyParser = require('body-parser')
 const expressLayouts = require('express-ejs-layouts')
 const http = require('http');
 const WebSocket = require('ws');
-// const uuiders = require('uuid');
+const uuiders = require('uuid');
 // // var job = require('./cron.js');
 
 const homeRouter = require('./routes/home');
@@ -33,25 +33,56 @@ app.use(passportConfig.initialize());
 app.use(passportConfig.session());
 
 
-app.get('/',function(req,res){
+app.get('/', function (req, res) {
   res.render('index/first-visit');
 })
 
-app.use('/accounts',accountsRouter);
-app.use('/home',homeRouter);
+app.use('/accounts', accountsRouter);
+app.use('/home', homeRouter);
 
 const server = http.createServer(app);
-const wss = new WebSocket.Server({server, clientTracking:true})
+const wss = new WebSocket.Server({ server, clientTracking: true })
+const db = require('./config/database');
+let rooms = {};
 
-wss.on('connection',function(ws,req){
-  console.log("Someone joined");
-  ws.on("message",function(msg){
-    console.log(JSON.parse(msg));
-    ws.send(JSON.stringify({"aa":'aa'}))
+wss.on('connection', function (ws, req) {
+  let uuid = uuiders.v4();
+
+  ws.on("message", function (msg) {
+    let data = JSON.parse(msg);
+    switch (data.meta) {
+      case "join":
+        if (!rooms[data.uuid]) rooms[data.uuid] = {}; // create the room
+        if (!rooms[data.uuid][uuid]) rooms[data.uuid][uuid] = ws; //join the existing room
+        break;
+      case "leave":
+        leave(data.uuid);
+        break;
+      default:
+        if (data.type == 'res') {
+          db.query("INSERT INTO feeding_logs (pet_id,duration,status) VALUES (?,?,?)", [data.id, data.duration, "SUCCESS"]).catch((error) => console.log(error));
+        } else if (data.type == 'req') {
+          Object.entries(rooms[data.uuid]).forEach(([, sock]) => sock.send(JSON.stringify(data)));
+        }
+    }
   })
+
+  ws.on("close", () => {
+    Object.keys(rooms).forEach(room => leave(room));
+  })
+  
+  function leave(room) {
+    console.log("Start leave, length rooms: " + Object.keys(rooms).length);
+    if (!rooms[room][uuid]) return;
+    if (Object.keys(rooms[room]).length == 1) delete rooms[room];
+    else delete rooms[room][uuid];
+    console.log("End leave, length rooms: " + Object.keys(rooms).length);
+  }
+
 })
 
-server.listen(port, function(){
+
+server.listen(port, function () {
   console.log("App running at port " + port);
 })
 
